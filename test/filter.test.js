65e28c48566a29fede63ad9592499708dcc48029
MALIHA TASNIM <maliha.tasnim@tuni.fi>
2020-12-05 22:52:12 +0600

    Initiating tests

---
+++ test/filter.test.js
@@ -0,0 +1,22 @@
+import filter from '../src/filter.js'
+import chai from 'chai'
+
+describe('filter.js', () => {
+    it('case with simple function result in false element', () => {
+        chai.expect(filter(['euro', 'dollar'], (item, index, array) => item !== 'dollar')).to.deep.equal(['euro'])
+    })
+
+    it('case with simple function that result in true', () => {
+        chai.expect(filter(['euro'], (item, index, array) => item === 'euro')).to.deep.equal(['euro'])
+    })
+
+    it('invalid array should throw error', () => {
+        chai.expect(() => filter(1232, () => {
+            return false
+        })).to.throw(TypeError)
+    })
+
+    it('predicate not being function should throw error', () => {
+        chai.expect(() => filter([23], 'not func')).to.throw(TypeError)
+    })
+})
